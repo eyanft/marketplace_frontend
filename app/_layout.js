a9a3f1597c9a8a904a28ec62acb043e78f457812
eyanft <100683165+eyanft@users.noreply.github.com>
2025-04-07 09:19:07 +0100

    adding product form

--- app/_layout.js
+++ app/_layout.js
@@ -1,5 +1,13 @@
 import { Stack } from "expo-router";
 import "../config/global.css";
+
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack 
+      screenOptions={{ 
+        headerShown: false,
+        animation: 'slide_from_right' 
+      }} 
+    />
+  );
 }
