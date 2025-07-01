6460d9acc6c5018ef8102bb8b40e04ce782faa55
eyanft <100683165+eyanft@users.noreply.github.com>
2025-07-01 23:18:58 +0100

    update product detail styling

--- src/components/items/ProductInfo.js
+++ src/components/items/ProductInfo.js
@@ -5,7 +5,6 @@ import Reviews from "../items/Reviews";
 export default function ProductInfo({ product, onRatingPress }) {
   return (
     <View style={styles.productInfo}>
-      <Text style={styles.brand}>{product?.name}</Text>
       <Text style={styles.name}>{product?.seller.name}</Text>
       <Reviews
         rating={product?.rating}
@@ -23,10 +22,6 @@ export default function ProductInfo({ product, onRatingPress }) {
 const styles = StyleSheet.create({
   productInfo: {
     marginBottom: 16,
-  },
-  brand: {
-    fontSize: 20,
-    marginBottom: 2,
   },
   name: {
     fontSize: 14,
