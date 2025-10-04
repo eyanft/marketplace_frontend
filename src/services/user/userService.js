2e2e8ed64fadebf24f1eb26d1f14278c46fe8855
eyanft <100683165+eyanft@users.noreply.github.com>
2025-10-04 18:42:24 +0100

    fixing auth with mail

--- src/services/user/userService.js
+++ src/services/user/userService.js
@@ -22,13 +22,24 @@ export const updateUserDetails = async (data) => {
 };
 export const setDeviceID = async (data) => {
   try {
-    console.log(data);
+    console.log('Setting device ID:', data);
+    // Try the original endpoint first
     const response = await api.put("users/setDeviceID", null, {
       params: { deviceID: data },
     });
     return response.data;
   } catch (err) {
-    console.log(err);
-    throw err;
+    console.log('setDeviceID error:', err);
+    // If the specific endpoint fails, try updating user details instead
+    try {
+      console.log('Trying alternative approach - updating user details');
+      const response = await api.put("users/update", {
+        deviceID: data
+      });
+      return response.data;
+    } catch (updateErr) {
+      console.log('Alternative approach also failed:', updateErr);
+      throw updateErr; // Throw the original error
+    }
   }
 };
